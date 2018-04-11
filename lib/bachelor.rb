41e2b43d2addcfc9b3325e25cd1c52685274469f
Michelle Blevins <mjblevins7@gmail.com>
2018-04-11 05:05:00 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -57,6 +57,7 @@ def get_average_age_for_season(data, season)
         end
     end
   end
-  average = ages.inject(0.0).to_f { |sum, el| sum + el } / ages.size
+  sum = ages.inject(:+).to_f { |sum, el| sum + el } / a.size
+  average = sum / ages.size
   return average
 end
