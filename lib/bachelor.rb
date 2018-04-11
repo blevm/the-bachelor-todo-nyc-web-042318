a2d8f22130fded3b60940bce9419310c70ca297d
Michelle Blevins <mjblevins7@gmail.com>
2018-04-11 04:54:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,7 +52,7 @@ def get_average_age_for_season(data, season)
 
   data[season].each do |contestant|
     contestant.each do |stat, stat_value|
-        if stat == contestant["age"] 
+        if stat == contestant["age"]
           ages << stat_value.to_i
         end
     end
