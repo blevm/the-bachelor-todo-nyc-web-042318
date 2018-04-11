e7524ef57df9bd1f076ebe412c0bcf5a93590e84
Michelle Blevins <mjblevins7@gmail.com>
2018-04-11 04:14:29 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |which_season, contestant|
    if which_season == season
      contestant.each do |stat, stat_value|
-       binding.pry
+    #   binding.pry
        if "status" == "Winner"
          return "name"
        end
