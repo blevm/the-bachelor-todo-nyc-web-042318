59871fb7b990f2f49a6d32f6f701c51bdb9b2a1b
Michelle Blevins <mjblevins7@gmail.com>
2018-04-11 04:20:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,11 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data[season].each do |contestant|
+    if contestant["occupation"] == occupation
+      return (contestant["name"])
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
